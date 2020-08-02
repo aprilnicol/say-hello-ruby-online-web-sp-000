4f2003b0cbd843c4861092a5ab6ae2be71e3020e
April Adams <illusionlace@gmail.com>
2020-08-02 23:03:02 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def say_hello (name, language = "Ruby")
-  puts "Hello #{name}! Hello #{language} Programmer!"
+def say_hello (name, language = "Ruby Programmer")
+  puts "Hello #{name}! Hello #{language}!}
 end
